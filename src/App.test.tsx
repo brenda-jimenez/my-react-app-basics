e5133f6d527a228189a25b6c7c6b2a4009de91b1
Brenda <brenda.jimenez.neria@gmail.com>
2025-09-18 11:28:57 +0100

    Deleting unnecessary files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Fetching some data via a custom hook/i);
   expect(linkElement).toBeInTheDocument();
 });
